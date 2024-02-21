e16724cbaa2653795d0b91a7d14e532465cb8ad0
Ermat <ealymbaev@gmail.com>
2024-02-21 18:15:13 +0600

    Switch Blockchair API to BlocksDecoded proxy

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.4.7")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "2.2.1")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
