c24bbd03c0c89f74bce28a699eefb021d65bf351
Esenbek <esenbekkd@gmail.com>
2024-04-19 12:18:53 +0600

    Update BitcoinCashKit.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "2.2.1")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
